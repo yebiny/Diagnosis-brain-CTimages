ab67862a22b7b35c5c05fd5919db3ce03945e28f
yebiny <yebinyoun@gmail.com>
2020-04-08 16:40:39 +0900

    change base model

--- 4-Training/models.py
+++ 4-Training/models.py
@@ -5,7 +5,7 @@ from keras import optimizers, initializers, regularizers, metrics
 def get_model(model):
     return model_list[model]
 
-def vgg(x_shape):
+def base_model(x_shape):
     x = Input(shape=(x_shape[1], x_shape[2], x_shape[3]), dtype='float32', name='x')
     
     y = layers.Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal', kernel_regularizer=regularizers.l2(0.01))(x)
@@ -40,42 +40,9 @@ def your_model(x_shape):
     return models.Model(inputs=x, outputs=y)
 
 
-def base_model(x_shape):
-  
-  x = keras.Input(shape=(x_shape[1],x_shape[2],x_shape[3]))
-  
-  y = layers.Conv2D(8,(3,3), padding='same')(x)
-  y = layers.Activation('relu')(y)
-  y = layers.MaxPool2D(pool_size=(3,3))(y)
-  
-  y = layers.Conv2D(16,(3,3), padding='same')(y)
-  y = layers.Activation('relu')(y)
-  y = layers.MaxPool2D(pool_size=(3,3))(y)
-  
-  y = layers.Flatten()(y)
-  
-  y = layers.Dense(1024)(y)
-  y = layers.BatchNormalization()(y)
-  y = layers.Activation('relu')(y)
-
-  y = layers.Dense(128)(y)
-  y = layers.BatchNormalization()(y)
-  y = layers.Activation('relu')(y)
-  
-  y = layers.Dense(32)(y)
-  y = layers.BatchNormalization()(y)
-  y = layers.Activation('relu')(y)
-  
-  y = layers.Dense(1)(y)
-  y = layers.Activation('sigmoid')(y)
-  
-  return models.Model(inputs=x, outputs=y)
-
-
 model_list={
 'base_model':base_model,
 'your_model':your_model,
-'vgg':vgg,
 }
 def  main():
     x_shape=(1,128,128,1)
